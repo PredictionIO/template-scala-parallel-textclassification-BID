33963ba5ee9e1361021af9ea9b56739166924b25
EmergentOrder <lecaran@gmail.com>
2015-08-04 15:53:28 -0300

    experiment with replacing IDF transform with SPPMI transform and subsequent vector averaging

--- src/main/scala/org/template/textclassification/Preparator.scala
+++ src/main/scala/org/template/textclassification/Preparator.scala
@@ -5,8 +5,8 @@ import io.prediction.controller.PPreparator
 import io.prediction.controller.Params
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.feature.{IDF, IDFModel, HashingTF}
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.{Matrix, DenseMatrix, Vector, Vectors}
+import org.apache.spark.mllib.linalg.distributed._
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 
@@ -20,9 +20,8 @@ import scala.math._
 // components.
 
 case class PreparatorParams(
-  nGram : Int
-) extends Params
-
+                             nGram: Int
+                             ) extends Params
 
 
 // 2. Initialize your Preparator class.
@@ -30,44 +29,111 @@ case class PreparatorParams(
 class Preparator(pp: PreparatorParams) extends PPreparator[TrainingData, PreparedData] {
 
   // Prepare your training data.
-  def prepare(sc : SparkContext, td: TrainingData): PreparedData = {
-    new PreparedData(td, pp.nGram)
+  def prepare(sc: SparkContext, td: TrainingData): PreparedData = {
+    new PreparedData(td, pp.nGram, sc)
   }
 }
 
 //------PreparedData------------------------
 
-class PreparedData (
-val td : TrainingData,
-val nGram : Int
-) extends Serializable {
-
+class PreparedData(
+                    val td: TrainingData,
+                    val nGram: Int,
+                    @transient val sc: SparkContext
+                    ) extends Serializable {
 
 
   // 1. Hashing function: Text -> term frequency vector.
 
-  private val hasher = new HashingTF()
+  private val hasher = new HashingTF(2000)
 
-  private def hashTF (text : String) : Vector = {
-    val newList : Array[String] = text.split(" ")
-    .sliding(nGram)
-    .map(_.mkString)
-    .toArray
+  private def hashTF(text: String): Vector = {
+    val newList: Array[String] = text.split(" ")
+      .sliding(nGram)
+      .map(_.mkString)
+      .toArray
 
     hasher.transform(newList)
   }
 
+
+  private def calculateSPPMI(localMat: Matrix, N: Long, k: Int): IndexedSeq[MatrixEntry] = {
+
+    println(localMat)
+    val pmiMatrixEntries = for (i <- 0 until localMat.numCols; j <- 0 until localMat.numRows)
+      yield {
+        new MatrixEntry(j, i, math.max(0, math.log(localMat(j, i) * N / (localMat(i, i) * localMat(j, j))) / math.log(2.0) - math.log(k) / math.log(2.0)))
+      }
+    return pmiMatrixEntries
+  }
+
+  private def generateSPPMIMatrix(trainData: TrainingData, sc:SparkContext) : Map[String,Vector] = {
+    val hashedFeats = trainData.data.collect.map(e => hashTF(e.text))
+    val indexedRows = hashedFeats.zipWithIndex.map(x => new IndexedRow(x._2, x._1))
+
+
+    val blockMat: BlockMatrix = new IndexedRowMatrix(sc.parallelize(indexedRows)).toBlockMatrix
+
+    //println(blockMat.numCols())
+    //println(blockMat.numRows())
+    val cooccurrences = blockMat.transpose.multiply(blockMat)
+    val locMat = cooccurrences.toLocalMatrix
+    val k = 1
+
+    val pmiEntries = calculateSPPMI(locMat, blockMat.numRows, k)
+
+    val pmiMat: CoordinateMatrix = new CoordinateMatrix(sc.parallelize(pmiEntries))
+
+    val indexedPMIMat = pmiMat.toIndexedRowMatrix()
+
+    println(trainData.data.collect.size)
+    println(indexedPMIMat.rows.collect.size)
+
+    val pmiMatRows = indexedPMIMat.rows.collect().map(e=> e.index -> e.vector).toMap
+
+    //TODO: take into account feature counts, currently it's on/off
+    //also not use var
+    //j<- 0 until v(i).toIn
+    val composedWordVectors = for(v<- hashedFeats)
+      yield {
+        var ar =  Array.fill[Double](pmiMatRows.head._2.size)(0)
+        for( i <- 0 until v.size; if v(i) > 0){
+          ar = (ar,pmiMatRows(i).toArray).zipped.map(_ + _)
+          //ar = ar ++ pmiMatRows(i).toArray
+        }
+        Vectors.dense(ar.map(x=> x/v.size)).toSparse }
+
+    val textToSPPMIVectorMap = (trainData.data.map(x=> x.text).collect() zip composedWordVectors).toMap
+
+    return textToSPPMIVectorMap
+  }
+
+  val ppmiMap = generateSPPMIMatrix(td,sc)
+  println(ppmiMap.head._2.size)
+
   // 2. Term frequency vector -> t.f.-i.d.f. vector.
 
-  val idf : IDFModel = new IDF().fit(td.data.map(e => hashTF(e.text)))
+  //val idf : IDFModel = new IDF().fit(td.data.map(e => hashTF(e.text)))
 
 
   // 3. Document Transformer: text => tf-idf vector.
 
-  def transform(text : String): Vector = {
-    // Map(n-gram -> document tf)
-    idf.transform(hashTF(text))
-  }
+  //  def transform(text : String): Vector = {
+  //    // Map(n-gram -> document tf)
+  //    idf.transform(hashTF(text))
+  //  }
+
+
+
+   def transform(text : String): Vector = {
+      // Map(n-gram -> document tf)
+
+      val result = ppmiMap(text)
+      //println(result)
+      result
+    }
+
+
 
 
   // 4. Data Transformer: RDD[documents] => RDD[LabeledPoints]
@@ -79,7 +145,6 @@ val nGram : Int
 
   // 5. Finally extract category map, associating label to category.
   val categoryMap = td.data.map(e => (e.label, e.category)).collectAsMap
-
 
 
 }
